7d27e75408aecde48c877b09b94e9d1fef766179
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-09-26 14:33:35 -0300

    Actualización Entidad Estudiante

--- PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Controllers/ExamenController.java
+++ PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Controllers/ExamenController.java
@@ -1,4 +1,5 @@
 package com.example.PreuTopEducation.Controllers;
 
+
 public class ExamenController {
 }
--- PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Entities/Estudiante.java
+++ PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Entities/Estudiante.java
@@ -1,4 +1,43 @@
 package com.example.PreuTopEducation.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Estudiante {
+    @Id
+    private Long rut;
+    private String nombres;
+    private String apellidos;
+    private LocalDate fecha_nacimiento;
+    private String tipo_colegio_proc;
+    private String nombre_colegio;
+    private int año_egreso;
+    private String tipo_pago;
+    private int arancel_estudiante;
+    private int cantidad_cuotas;
+    private int promedio_examen;
+
+
+    public Estudiante(Long rut, String nombres, String apellidos, LocalDate fecha_nacimiento, String tipo_colegio_proc, String nombre_colegio, int año_egreso) {
+        this.rut = rut;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.tipo_colegio_proc = tipo_colegio_proc;
+        this.nombre_colegio = nombre_colegio;
+        this.año_egreso = año_egreso;
+    }//constructor con datos para ingreso al sistema
+
+
 }
