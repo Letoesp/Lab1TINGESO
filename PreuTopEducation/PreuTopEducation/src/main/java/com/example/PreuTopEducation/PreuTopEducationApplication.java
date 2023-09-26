a12e298aac1af969725c77705efba341a58d0692
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-09-26 13:09:29 -0300

    Inicialización Proyecto Spring

---
+++ PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/PreuTopEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.PreuTopEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreuTopEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreuTopEducationApplication.class, args);
+	}
+
+}
---
+++ PreuTopEducation/PreuTopEducation/src/test/java/com/example/PreuTopEducation/PreuTopEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PreuTopEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreuTopEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
