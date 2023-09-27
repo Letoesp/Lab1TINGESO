5cec7a1352e7c263bee4aba53f5f7cfe59ca189d
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-09-27 12:17:22 -0300

    Cambios en Controlador Estudiante

--- PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Controllers/EstudianteController.java
+++ PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Controllers/EstudianteController.java
@@ -1,4 +1,31 @@
 package com.example.PreuTopEducation.Controllers;
 
+import org.springframework.ui.Model;
+import com.example.PreuTopEducation.Entities.Estudiante;
+import com.example.PreuTopEducation.Services.EstudianteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller//para solicitudes html
+@RequestMapping
 public class EstudianteController {
+    private final EstudianteService estudianteService;
+    @Autowired //inyectar clase en constructor
+    public EstudianteController(EstudianteService estudianteService) {
+        this.estudianteService = estudianteService;
+    }
+
+    @GetMapping
+    public String getEstudiantes(Model model) {
+        List<Estudiante> estudiantes = estudianteService.getEstudiantes();
+        model.addAttribute("estudiantes", estudiantes);
+        return "registrar-estudiante";
+    }
+
+
+
 }
