875f557e75814ca528c9d45637dcb5842457d437
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-10-02 22:17:49 -0300

    cambio en repo cuota

--- PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Repositories/CuotaRepository.java
+++ PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Repositories/CuotaRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 @Repository
 public interface CuotaRepository extends JpaRepository<Cuota,Long> {
     Cuota findCuotaByIdcuota(Long idcuota);
