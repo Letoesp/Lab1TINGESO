c4006ece349b8e22ce950e936a25286e66c02c2a
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-10-07 18:12:17 -0300

    Implementación subir notas

--- PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Controllers/ExamenController.java
+++ PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Controllers/ExamenController.java
@@ -1,9 +1,45 @@
 package com.example.PreuTopEducation.Controllers;
+import com.example.PreuTopEducation.Services.ExamenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
+import java.io.IOException;
 
 
 @Controller
 public class ExamenController {
+    private final ExamenService examenService;
+
+    @Autowired
+    public ExamenController(ExamenService examenService) {
+        this.examenService = examenService;
+    }
+
+    @GetMapping("/subir_notas")
+    public String mostrarFormularioSubirNotas() {
+        return "subir_notas";
+    }
+
+    @PostMapping("/subir_notas")
+    public String subirNotas(@RequestParam("archivo") MultipartFile archivo, Model model) {
+        try {
+            examenService.cargarNotasDesdeCSV(archivo);
+            // Redirige a alguna página de confirmación
+            model.addAttribute("mensaje", "Notas subidas exitosamente.");
+        } catch (IOException e) {
+            // Maneja las excepciones según tus necesidades
+            e.printStackTrace();
+            model.addAttribute("mensaje", "Error al subir las notas. Por favor, revisa el archivo e inténtalo nuevamente.");
+        }
+        return "redirect:/subir_notas";
+    }
+
 
 
 }
--- PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Services/ExamenService.java
+++ PreuTopEducation/PreuTopEducation/src/main/java/com/example/PreuTopEducation/Services/ExamenService.java
@@ -1,17 +1,55 @@
 package com.example.PreuTopEducation.Services;
+import com.example.PreuTopEducation.Entities.Estudiante;
+import com.example.PreuTopEducation.Entities.Examen;
 import com.example.PreuTopEducation.Repositories.ExamenRepository;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-i
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.time.LocalDate;
+
 @Service
 public class ExamenService {
 
     private final ExamenRepository examenRepository;
+    private final EstudianteService estudianteService;
+
+    @Autowired
     public ExamenService(ExamenRepository examenRepository, EstudianteService estudianteService) {
         this.examenRepository = examenRepository;
-
+        this.estudianteService = estudianteService;
     }
 
+    public void cargarNotasDesdeCSV(MultipartFile archivo) throws IOException {
+        try (Reader reader = new BufferedReader(new InputStreamReader(archivo.getInputStream()));
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
+
+            for (CSVRecord csvRecord : csvParser) {
+                Long rutEstudiante = Long.parseLong(csvRecord.get("rutEstudiante"));
+                String fechaExamen = csvRecord.get("fechaExamen");
+                LocalDate fecha = LocalDate.parse(fechaExamen); // Asegúrate de que el formato de fecha en el CSV sea correcto
+                int puntaje = Integer.parseInt(csvRecord.get("puntajeObtenido"));
+
+                // Buscar estudiante por rut
+                Estudiante estudiante = estudianteService.obtenerEstudianteporRut(rutEstudiante);
+
+                // Crea un objeto Examen y guárdalo en la base de datos
+                Examen examen = new Examen();
+                examen.setEstudiante(estudiante);
+                examen.setFecha(fecha);
+                examen.setPuntaje(puntaje);
+                examenRepository.save(examen);
+            }
+        }
+    }
 
 }
 
